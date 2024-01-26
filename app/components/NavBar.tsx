b9fad1b6933f4d78086d372b8a70e63750f6a936
denzubechi <chukwumasamuel371@gmail.com>
2024-01-25 18:04:05 -0800

    feat:completed navbar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -10,15 +10,14 @@ const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
 
     return (
-        <header className="bg-background w-full py-4 px-6 border-b border-gray-200 bg-gray-50 dark:bg-black dark:border-gray-700 absolute mx-auto z-50 ">
+        <header className="bg-background w-full py-2 lg:py-4  px-6 border-b border-gray-200 bg-gray-50 dark:bg-black dark:border-gray-700 absolute mx-auto z-50 ">
             <div className="flex items-center justify-between mx-auto w-full">
                 <span className='flex items-center gap-4'>
-                    <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 40 40" fill="none">
                             <path fill-rule="evenodd" clip-rule="evenodd" d="M20 40C31.0457 40 40 31.0457 40 20C40 8.95431 31.0457 0 20 0C8.95429 0 0 8.95431 0 20C0 31.0457 8.95429 40 20 40ZM26.2393 9.31684C26.543 8.23805 25.4961 7.60013 24.54 8.2813L11.1931 17.7896C10.1562 18.5283 10.3193 20 11.4381 20H14.9527V19.9728H21.8025L16.2212 21.9421L13.7607 30.6832C13.457 31.762 14.5038 32.3999 15.46 31.7187L28.8069 22.2105C29.8438 21.4718 29.6806 20 28.5619 20H23.2321L26.2393 9.31684Z" fill="#34CAA5" />
                         </svg>
-                    </div>
                     <h1 className="text-md md:text-xl font-semibold hidden md:block">Dashboard</h1>
+                    <h1 className="text-lg font-semibold sm:block sm:text-xl lg:hidden">Dashboard</h1>
                 </span>
 
                 <div
@@ -27,23 +26,17 @@ const Navbar = () => {
                     <Menu as="div" className="relative inline-block text-left">
                             {({ open }) => (
                                 <>
-                                    <Menu.Button className="cursor-pointer focus:outline-none flex items-center space-x-3 p-2 mx-2 border rounded-full">
+                                    <Menu.Button className="cursor-pointer focus:outline-none flex items-center space-x-3 p-2  mx-2 border border-gray-400 dark:border-gray-500 rounded-full">
                                         
-                                        <span className='rounded-full border border-gray-300 dark:border-gray-400'>
+                                        <span className='rounded-full border border-gray-300 dark:border-gray-500'>
                                             <Image 
-                                            className="w-10 h-10"
+                                            className="w-8 h-8"
                                             src={Avatar}
                                             alt="sun icon"
                                             />
                                         </span>
-                                        <div className="flex flex-col">
-                                        <span>Sam Dave</span>
-                                        <span className='text-gray-400'>
-                                        sam@gmail.com
-                                        </span>
-                                    </div>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                                    <path d="M3.19841 6.20675C3.43891 5.95614 3.81525 5.93336 4.08045 6.1384L4.15643 6.20675L10 12.2955L15.8436 6.20675C16.0841 5.95614 16.4604 5.93336 16.7256 6.1384L16.8016 6.20675C17.0421 6.45735 17.064 6.84951 16.8672 7.12585L16.8016 7.20502L10.479 13.7933C10.2385 14.0439 9.86217 14.0666 9.59697 13.8616L9.52099 13.7933L3.19841 7.20502C2.93386 6.92935 2.93386 6.48241 3.19841 6.20675Z" fill="#0D062D"className='fill-gray-700 dark:fill-gray-400'/>
+                                    <path d="M3.19841 6.20675C3.43891 5.95614 3.81525 5.93336 4.08045 6.1384L4.15643 6.20675L10 12.2955L15.8436 6.20675C16.0841 5.95614 16.4604 5.93336 16.7256 6.1384L16.8016 6.20675C17.0421 6.45735 17.064 6.84951 16.8672 7.12585L16.8016 7.20502L10.479 13.7933C10.2385 14.0439 9.86217 14.0666 9.59697 13.8616L9.52099 13.7933L3.19841 7.20502C2.93386 6.92935 2.93386 6.48241 3.19841 6.20675Z" fill="#0D062D"className='fill-gray-500 dark:fill-gray-400'/>
                                     </svg>
                                     </Menu.Button>
 
@@ -57,14 +50,14 @@ const Navbar = () => {
                                         leaveFrom="transform opacity-100 scale-100"
                                         leaveTo="transform opacity-0 scale-95"
                                     >
-                                        <Menu.Items static className="absolute right-0 w-56 z-50 mt-2 origin-top-right bg-white rounded-lg   shadow-lg">
+                                        <Menu.Items static className="absolute right-0 w-56 z-50 mt-2 px-2 origin-top-right bg-white dark:bg-gray-900 border dark:border-gray-800 rounded-lg shadow-xl">
                                             <div className="py-1">
                                                 <Menu.Item>
                                                     {({ active }) => (
                                                         <a
                                                             href="#"
                                                             className={`${
-                                                                active ? "bg-gray-100 text-gray-900" : "text-gray-700"
+                                                                active ? "bg-green-500 text-gray-200" : "text-gray-500 dark:text-gray-300"
                                                             } group flex rounded-md items-center w-full px-4 py-2 text-sm`}
                                                         >
                                                             Profile
@@ -76,10 +69,10 @@ const Navbar = () => {
                                                         <a
                                                             href="#"
                                                             className={`${
-                                                                active ? "bg-gray-100 text-gray-900" : "text-gray-700"
+                                                                active ? "bg-green-500 text-gray-200" : "text-gray-500 dark:text-gray-300"
                                                             } group flex rounded-md items-center w-full px-4 py-2 text-sm`}
                                                         >
-                                                            Settings
+                                                            Report Bug
                                                         </a>
                                                     )}
                                                 </Menu.Item>
@@ -90,10 +83,10 @@ const Navbar = () => {
                                                         <a
                                                             href="#"
                                                             className={`${
-                                                                active ? "bg-gray-100 text-gray-900" : "text-gray-700"
+                                                                active ? "bg-green-500 text-gray-200" : "text-gray-500 dark:text-gray-300"
                                                             } group flex rounded-md items-center w-full px-4 py-2 text-sm`}
                                                         >
-                                                            Sign out
+                                                           Refer and earn
                                                         </a>
                                                     )}
                                                 </Menu.Item>
@@ -112,7 +105,6 @@ const Navbar = () => {
                 </div>
 
                 <div className="hidden md:flex items-center space-x-4">
-
                     <div className='flex items-center space-x-3'>
                         <div className="relative">
                             <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -121,7 +113,7 @@ const Navbar = () => {
                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M8.68945 1C12.9293 1 16.3781 4.3727 16.3781 8.51907C16.3781 10.4753 15.6104 12.2595 14.3542 13.5986L16.8261 16.0109C17.0574 16.2371 17.0582 16.6031 16.8269 16.8294C16.7116 16.9436 16.5592 17 16.4076 17C16.2568 17 16.1052 16.9436 15.9892 16.8309L13.4874 14.3912C12.1714 15.4219 10.5028 16.0389 8.68945 16.0389C4.44955 16.0389 1 12.6655 1 8.51907C1 4.3727 4.44955 1 8.68945 1ZM8.68945 2.15821C5.10251 2.15821 2.18433 5.01125 2.18433 8.51907C2.18433 12.0269 5.10251 14.8807 8.68945 14.8807C12.2756 14.8807 15.1938 12.0269 15.1938 8.51907C15.1938 5.01125 12.2756 2.15821 8.68945 2.15821Z" fill="#78828A"/>
                                 </svg>
                             </div>
-                            <input type="search" id="default-search" className="block w-full p-4  py-3 pl-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." required />
+                            <input type="search" id="default-search" className="outline-none block w-full p-4  py-3 pl-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-green-500 focus:border-green-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="Search..." required />
                         </div>
                     </div>
                     <div className="flex items-center space-x-3">
@@ -194,7 +186,7 @@ const Navbar = () => {
                                                                 active ? "bg-gray-100 text-gray-900" : "text-gray-700"
                                                             } group flex rounded-md items-center w-full px-4 py-2 text-sm`}
                                                         >
-                                                            Settings
+                                                            Report Bug
                                                         </a>
                                                     )}
                                                 </Menu.Item>
@@ -208,7 +200,7 @@ const Navbar = () => {
                                                                 active ? "bg-gray-100 text-gray-900" : "text-gray-700"
                                                             } group flex rounded-md items-center w-full px-4 py-2 text-sm`}
                                                         >
-                                                            Sign out
+                                                            Refer and Earn
                                                         </a>
                                                     )}
                                                 </Menu.Item>
@@ -251,14 +243,26 @@ const Navbar = () => {
             </div>
                   <div className="px-4 space-y-5">
                       {/* Drawer content here */}
+
+                      <div className=" cursor-pointer sm:flex md:flex items-center space-x-3 border border-gray-300 dark:gray-300 rounded-full p-2">
+  <div className="flex items-center space-x-2">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 20 20" fill="none">
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M10.0001 1.04163C8.2872 1.04163 6.64449 1.72206 5.43332 2.93323C4.22215 4.1444 3.54172 5.7871 3.54172 7.49996V8.08663C3.54167 8.66737 3.36973 9.23511 3.04755 9.71829L2.09172 11.1541C0.980053 12.8208 1.82839 15.0858 3.76089 15.6125C4.39005 15.7841 5.02505 15.9291 5.66422 16.0483L5.66589 16.0525C6.30589 17.7625 8.01839 18.9583 10.0001 18.9583C11.9817 18.9583 13.6942 17.7625 14.3351 16.0525L14.3367 16.0483C14.9769 15.9292 15.6119 15.7838 16.2401 15.6125C18.1726 15.0858 19.0209 12.8208 17.9092 11.1541L16.9526 9.71829C16.6304 9.23511 16.4584 8.66737 16.4584 8.08663V7.49996C16.4584 5.7871 15.778 4.1444 14.5668 2.93323C13.3556 1.72206 11.7129 1.04163 10.0001 1.04163ZM12.8134 16.2808C10.9442 16.5041 9.05507 16.5041 7.18589 16.2808C7.77839 17.1316 8.80922 17.7083 10.0001 17.7083C11.1909 17.7083 12.2209 17.1316 12.8134 16.2808ZM4.79172 7.49996C4.79172 6.11862 5.34045 4.79386 6.31721 3.81711C7.29396 2.84036 8.61872 2.29163 10.0001 2.29163C11.3814 2.29163 12.7062 2.84036 13.6829 3.81711C14.6597 4.79386 15.2084 6.11862 15.2084 7.49996V8.08663C15.2084 8.91412 15.4534 9.72329 15.9126 10.4116L16.8692 11.8475C17.0175 12.0695 17.1108 12.3235 17.1415 12.5887C17.1722 12.8539 17.1393 13.1226 17.0457 13.3726C16.9521 13.6226 16.8004 13.8467 16.6031 14.0265C16.4057 14.2063 16.1685 14.3366 15.9109 14.4066C12.0407 15.4621 7.95855 15.4621 4.08839 14.4066C3.83103 14.3364 3.59403 14.206 3.39692 14.0263C3.19981 13.8465 3.04822 13.6225 2.95464 13.3727C2.86106 13.1228 2.82816 12.8544 2.85866 12.5893C2.88915 12.3243 2.98217 12.0703 3.13005 11.8483L4.08839 10.4116C4.54717 9.72303 4.79189 8.91406 4.79172 8.08663V7.49996Z" fill="#0D062D" className='fill-gray-800 dark:fill-gray-300'/>
+    </svg>
+    <span className="text-sm">Notifications</span>
+  </div>
+</div>
+                      <hr className="my-2 border-gray-300 dark:border-gray-700" />
                       <div className='flex items-center space-x-3'>
+                                                           
+                      
       <div className="relative">
         <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
           <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
             <path fill-rule="evenodd" clip-rule="evenodd" d="M8.68945 1C12.9293 1 16.3781 4.3727 16.3781 8.51907C16.3781 10.4753 15.6104 12.2595 14.3542 13.5986L16.8261 16.0109C17.0574 16.2371 17.0582 16.6031 16.8269 16.8294C16.7116 16.9436 16.5592 17 16.4076 17C16.2568 17 16.1052 16.9436 15.9892 16.8309L13.4874 14.3912C12.1714 15.4219 10.5028 16.0389 8.68945 16.0389C4.44955 16.0389 1 12.6655 1 8.51907C1 4.3727 4.44955 1 8.68945 1ZM8.68945 2.15821C5.10251 2.15821 2.18433 5.01125 2.18433 8.51907C2.18433 12.0269 5.10251 14.8807 8.68945 14.8807C12.2756 14.8807 15.1938 12.0269 15.1938 8.51907C15.1938 5.01125 12.2756 2.15821 8.68945 2.15821Z" fill="#78828A"/>
           </svg>
         </div>
-        <input type="search" id="default-search" className="block w-full p-4 py-3 pl-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." required />
+        <input type="search" id="default-search" className="outline-none block w-full p-4 py-3 pl-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-green-500 focus:border-green-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="Search..." required />
       </div>
     </div>
 
@@ -270,6 +274,8 @@ const Navbar = () => {
       </svg>
       <span className="text-sm">November 15, 2023</span>
     </div>
+
+  
                   </div>
                 </div>
             </Transition>
